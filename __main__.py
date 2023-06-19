0c3f6565e75d158c798d7e8ffc8bcfb0a1a7f620
Đoàn Ngọc Bình Minh <116936560+MIT4893-Projects@users.noreply.github.com>
2023-06-19 19:08:31 +0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-START_ID = 5001
-STOP_ID = 5999
+START_ID = 0
+STOP_ID = 100000
 
 BASE_URL = "https://diemthi.hcm.edu.vn/home/show"
 
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     f= open("output.out", "w+")
     for i in range(START_ID, STOP_ID + 1):
         data = {
-            "sobaodanh" : str(i).rjust(5, '0')
+            "sobaodanh" : str(i)
         }
         result = requests.post(BASE_URL, json=data)
         if (result.status_code != 200):
@@ -21,4 +21,4 @@ if __name__ == "__main__":
             name, dob, point = [i.contents[0].strip("\n\r ") for i in infomation.find_all('td')]
             print(i, name, dob, point, file=f, sep=" | ")
         except:
-            continue
+            print(f"Can't get data with id: {data["sobaodanh"]})
