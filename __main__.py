039f8a37edea89ad69f86eb4e9dc9dafb5d51c5a
Đoàn Ngọc Bình Minh <116936560+MIT4893-Projects@users.noreply.github.com>
2024-06-19 04:11:15 +0000

    update

--- __main__.py
+++ __main__.py
@@ -1,57 +1,64 @@
-from bs4 import BeautifulSoup
+import csv
+import threading
 import requests
 from logger import Logger
-from plot_generator import PlotGenerator
-from contestant import Contestant
-from points import Points
 
-START_ID = 0
-STOP_ID = 100000
+BASE_URL = "https://s6.tuoitre.vn/api/diem-thi-lop-10.htm"
+HEADERS = {"Origin": "https://tuoitre.vn"}
 
-BASE_URL = "https://diemthi.hcm.edu.vn/home/show"
+THREAD_TASKS = [
+    [[9001, 9999, 'result1.csv'], [90000, 99999, 'result1.csv']],
+    [[100000, 109999, 'result2.csv']],
+    [[110000, 119999, 'result3.csv']],
+    [[129999, 130000, 'result4.csv']]
+]
 
-PLOT = PlotGenerator()
+def crawl(start, stop, file):
+    f = open(file, "a")
+    writer = csv.writer(f)
+    session = requests.Session()
+    session.headers.update(HEADERS)
 
-def crawl(start, stop, isspec=False):
-    for i in range(start, stop + 1):
-        data = {
-            "sobaodanh": str(i)
-        }
+    for id in range(start, stop + 1):
+        params = {"keywords": str(id), "year": "2024"}
 
-        result = requests.post(BASE_URL, json=data)
-        if (result.status_code != 200):
-            continue
-        
-        Logger.log_message(f"Crawling: id = {i}")
-        soup = BeautifulSoup(result.text)
         try:
-            information = soup.body.find("div", class_="container").table.find_all("tr")[1]
+            response = session.get(BASE_URL, params=params)
+            data = response.json()['data'][0]
             
-            table_row = information.find_all("td")
-            full_name = table_row[0].text.split("_")[-1].strip("\r\n ").title()
-            point_information = table_row[1].text.strip("\r\n ").split(" ")
+            points = (
+                data['id'],
+                data['HO_TEN'],
+                data['TOAN'],
+                data['VAN'], 
+                data['NGOAI_NGU'],
+                data['CHUYEN'],
+                sum((data['TOAN'], data['VAN'], data['NGOAI_NGU'])),
+                sum((data['TOAN'], data['VAN'], data['NGOAI_NGU'], 2 * data['CHUYEN'] if data['CHUYEN'] > 0 else 0)),
+            )
 
+            writer.writerow(points)
 
-            points = []
-            for point in point_information:
-                try:
-                    points.append(float(point.replace(",", ".")))
-                except ValueError:
-                    continue
-                    
-            while (len(points) < 4):
-                points.append(0.0)
-            contestant = Contestant(i, full_name, Points(points[0], points[1], points[2], points[3], isspec))
-            PLOT.add_contestant(contestant)
-            
-            
-        except AttributeError as error:
-            Logger.log_error(error.name)
-            Logger.log_message(f"Can't get data with id: {data['sobaodanh']}")
-            Logger.log_message("Terminate this batch. Move to next batch or exit if don't have any batch left.")
-            return
+            Logger.log_message(f"SUCCESS - ID: {id}")
+        except:
+            Logger.log_warning(f"FAILED - ID: {id}")
+
+    f.close()
+
+def thread(tasks):
+    for task in tasks:
+        crawl(*task)
 
 if __name__ == "__main__":
-    crawl(9000, 9999)
-    crawl(90000, 99999)
-    crawl(100000, 199999)
+    threads = []
+
+    for task in THREAD_TASKS:
+        t = threading.Thread(target=thread, args=(task,))
+        threads.append(t)
+        t.start()
+
+        Logger.log_message(f"STARTED - THREAD: {task}")
+    
+    for t in threads:
+        t.join()
+
